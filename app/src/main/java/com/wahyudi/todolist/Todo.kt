8d0303d6aa9d37b061eb2d64aa240047d0a5c1bd
Wahyudi Sumara <yudi.sumara09@gmail.com>
2020-05-27 17:45:24 +0800

    ui dan model data

---
+++ app/src/main/java/com/wahyudi/todolist/Todo.kt
@@ -0,0 +1,33 @@
+package com.wahyudi.todolist
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todotable")
+data class Todo(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int? = null,
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "note")
+    var note: String? = null,
+
+    @ColumnInfo(name = "date_created")
+    var dateCreated: String,
+
+    @ColumnInfo(name = "date_updated")
+    var dateUpdated: String,
+
+    @ColumnInfo(name = "due_date")
+    var dueDate: String,
+
+    @ColumnInfo(name = "due_time")
+    var dueTime: String,
+
+    @ColumnInfo(name = "remind_me")
+    var remindMe: Boolean = true
+)
